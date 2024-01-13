2d8e468cf51078dee69137427f2f571f9ff3673a
Taitan <110903767+TaitanB@users.noreply.github.com>
2024-01-13 23:40:18 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">🙃 TALKIE  👉 <a href="https://teamchallenge-chat-api.onrender.com/api-docs/#/" target="_blank" rel="noreferrer">api-docs-link</a></h1>
+
+<h2 align="center"><img src="/tmp/talkie.svg" alt="favicon" width="36"><a href="https://talkiehub.netlify.app/" target="_blank" rel="noreferrer">TALKIE</a></h2>
+<p align="center">Chat, Connect, Stay In Touch And Make Communication Simple.</p>
+
+<h2 align="center">This project was created using:</h2>
+
+<div align="center">
+<a href="https://nodejs.org/ru" target="_blank" rel="noreferrer"><img src="/tmp/nodejs.png" alt="nodejs" width="48"></a> &nbsp;
+<a href="http://expressjs.com/en" target="_blank" rel="noreferrer"><img src="/tmp/icon-express-js.png" alt="expressjs" width="48"></a> &nbsp;
+<a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img src="/tmp/mongodb.png" alt="MongoDB" width="48"></a> &nbsp;
+<a href="https://swagger.io/docs/specification/about/" target="_blank" rel="noreferrer"><img src="/tmp/swagger.png" alt="swagger" width="48"></a>
+</div>
+
+<h2 align="center">TALKIE 👉 
+<a href="https://github.com/UlyanaKucherenko/TeamChallenge_Chat" target="_blank" rel="noreferrer">github-link</a></h2>
+
+![screenshot](tmp/Screenshot_3.jpg)
+
+January 2024
--- schemas/rooms.js
+++ schemas/rooms.js
@@ -20,33 +20,8 @@ const roomAddSchema = Joi.object({
   img: Joi.string(),
 });
 
-// const roomSchema = Joi.object({
-//   title: Joi.string().min(4).max(30).required().messages({
-//     "string.base": "The title must be a string of 4 to 30 symbols.",
-//     "string.min": "The title must be not less 4 symbols.",
-//     "string.max": "The title must be no more 30 symbols.",
-//     "any.required": "The title field is required.",
-//   }),
-//   description: Joi.string().max(300).messages({
-//     "string.max": "The description must be no more 300 symbols.",
-//   }),
-//   type: Joi.string().valid("public", "private").required().messages({
-//     "any.required": "The type is required.",
-//   }),
-//   topic: Joi.string().when("type", {
-//     is: "public",
-//     then: Joi.string()
-//       .valid(...Object.keys(topicsEnum))
-//       .required()
-//       .messages({
-//         "any.required": "The topic is required for public rooms.",
-//       }),
-//     otherwise: Joi.string().forbidden(),
-//   }),
-// });
-
 const roomEditSchema = Joi.object({
-  title: Joi.string().min(4).max(30).pattern(textRegex).required().messages({
+  title: Joi.string().min(2).max(30).pattern(textRegex).required().messages({
     "string.base": "The title must be a string of 4 to 30 symbols.",
     "string.min": "The title must be not less 4 symbols.",
     "string.max": "The title must be no more 30 symbols.",
